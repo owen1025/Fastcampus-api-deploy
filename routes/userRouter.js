2792856602373c547b29435e19ab83eab1a19e37
Jepil Choi <myartame@gmail.com>
2018-07-19 17:20:18 +0900

    .

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+const multer = require('multer')();
+
 /**
  * @api public
- * @method GET
- * @description 앨범 리스트 제공 API
- * @returns {json[array]}
+ * @method POST
+ * @description 로그인 API
+ * @param {(body) string} [email] 유저 이메일
+ * @param {(body) string} [password] 유저 패스워드
  */
-router.post('/login', (req, res) => {
+router.post('/login', multer.array(), (req, res) => {
+  const { email, password } = req.body;
+
   res
     .sendStatus(200);
+});
+
+/**
+ * @api public
+ * @method POST
+ * @description 회원가입 API
+ * @param {(body) string} [email] 유저 이메일
+ * @param {(body) string} [password] 유저 패스워드
+ */
+router.post('/join', multer.array(), (req, res) => {
+  const { email, password } = req.body;
+
+  res
+    .sendStatus(201);
 });
 
 module.exports = router;
